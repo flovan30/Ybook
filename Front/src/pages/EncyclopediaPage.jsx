a13637a5dfe2b6fb224f05a319c95a24d26a1412
Florian - vrst <87174742+flovan30@users.noreply.github.com>
2023-10-05 15:30:59 +0200

    encyclopedie

--- Front/src/pages/EncyclopediaPage.jsx
+++ Front/src/pages/EncyclopediaPage.jsx
@@ -1,10 +1,17 @@
+import Navbar from "../components/Navbar";
 import ListItem from "../components/ListItem";
 
 const EncyclopediaPage = () => {
   return (
     <>
+      <Navbar />
       <ListItem item="casque" />
       <ListItem item="armure" />
+      <ListItem item="gants" />
+      <ListItem item="pantalons" />
+      <ListItem item="bottes" />
+      <ListItem item="bouclier" />
+      <ListItem item="arme" />
     </>
   );
 };
