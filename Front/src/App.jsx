3c691505a8371e6a31dcfcf05af3dd647eb192d4
Florian - vrst <87174742+flovan30@users.noreply.github.com>
2023-10-05 09:27:34 +0200

    encyclopedie

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
+import EncyclopediaPage from "./pages/EncyclopediaPage";
 import ErrorPage from "./pages/ErrorPage";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/encyclopedie" element={<EncyclopediaPage />} />
 
         {/* page d'érreur */}
         <Route path="*" element={<ErrorPage />} />
